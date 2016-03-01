3dd3b4cd17f692ad80177b5022409664ddb27678
Student <Student@10.0.9.120>
2016-03-01 16:18:25 +0200

    Not done!

--- stam/src/stam/stam1.java
+++ stam/src/stam/stam1.java
@@ -5,6 +5,7 @@ public class stam1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		System.out.println("I'm not done yet");
 	}
 
 }
